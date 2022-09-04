7e61ae9148dba64820ae40e507d1c00e1e75dc96
ChrisKen15 <kennethchris99@gmail.com>
2022-09-04 13:17:32 -0400

    Updated

---
+++ Grocery Shopping/RunProject1.java
@@ -0,0 +1,14 @@
+package project1;
+/**
+This is a driver class to run project 1.
+ */
+public class RunProject1 {
+	
+	/**
+	The main method calls the run() method in the shopping class.
+	@param args is not used
+	 */
+	public static void main(String[] args) {
+		new Shopping().run();
+	}
+}
